da9b84cda6da3f8d86e1c1d72be9cbfac08a5b50
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-03-28 20:16:21 -0300

    23/03

---
+++ Aula2/Aula2.1/index.js
@@ -0,0 +1,16 @@
+const message = "olá";
+
+if(true){
+
+}
+
+
+
+for(const color of ['verde', 'vermelho', 'amarelo']){
+    const message = "olá";
+    console.log(color);
+    console.log(message);
+}
+
+console.log(color);
+console.log(message);
