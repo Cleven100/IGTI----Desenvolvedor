22095dbb2566d6131ff6492080a1d602f3a87e88
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-03-29 14:59:02 -0300

    29/03

--- Aula3/Aula3.2/index.js
+++ Aula3/Aula3.2/index.js
@@ -26,3 +26,19 @@ Promise.all([
 ]).then(results => results.length)
 .then(size => console.info(size))
 .catch(erro => console.error(erro));
+
+
+
+
+
+Promise.all([
+     
+      
+    new Promise(resolve => setTimeout(() => resolve([]), 2800)),
+    new Promise((resolve, reject) => setTimeout(() => reject([10]), 1200)),
+    new Promise(resolve => setTimeout(() => resolve([3,4]), 800))
+
+]).then(results => results.length)
+.then(size => console.info(size))
+.catch(erro => console.error(erro));
+
