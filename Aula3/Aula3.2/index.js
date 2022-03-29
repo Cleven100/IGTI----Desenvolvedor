ccb974be4d13c009af79d00c9a2f8dfbd4de3b8b
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-03-29 14:51:25 -0300

    29/03

--- Aula3/Aula3.2/index.js
+++ Aula3/Aula3.2/index.js
@@ -3,3 +3,14 @@ const p1 = new Promise((resolve) => resolve(console.log("Sempre será resolvida"
 Promise.resolve(console.log("Sempre será resolvida"));
 
 
+Promise.reject(console.log("sempre será rejeitada"));
+
+Promise.all([
+    
+    new Promise(resolve => setTimeout(resolve, 1200, "p1")),
+    new Promise(resolve => setTimeout(resolve, 1200, "p2")),
+    new Promise(resolve => setTimeout(resolve, 1200, "p3")),
+
+]).then(results => results.data[0].name)
+.then(name => console.info(name))
+.catch(error => console.error(`exceção lançada na: ${error}`))
