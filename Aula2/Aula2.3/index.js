d1f134df1a81ac3b151c35dd3d05484f7875edb6
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-03-29 11:11:11 -0300

    29/03

--- Aula2/Aula2.3/index.js
+++ Aula2/Aula2.3/index.js
@@ -3,10 +3,53 @@ let Carro = {
     ano: 2016
 }
 
+const handler = {
+    get(target, property, receiver) {
+        console.log(`GET ${property}`);
+        if(property in target){
+            return target[property];
+        }
+        return "Propriedade inexistente"
+        
+    }
+}
+
+let carroProxy = new Proxy(Carro, handler);
+
+console.log(Carro.modelo);
+console.log(carroProxy.modelo);
+
+let tradutor = {
+    'Carro': 'Car',
+    'Ano'  : 'Year'
+};
+
+let handlerTradutor = {
+    get(target, property){
+        if(property in target){
+            return target[property];
+        } else { 
+            return property;
+        }
+    },
+    set(target, property, value){
+      if(typeof value == 'string'){
+          target[property] = value;
+      } else {
+          return false;
+      }
+    }
+}
+let tradutorProxy = new Proxy(tradutor,handlerTradutor);
 
 
-let carroProxy = new Proxy(Carro, {});
+console.log(tradutorProxy['Carro']);
+console.log(tradutorProxy['Modelo']);
 
-console.log(Carro.ano);
-console.log(carroProxy.ano);
+tradutorProxy["Modelo"] = "Model";
+tradutorProxy["Marca"] = 4554541;
+
+
+console.log(tradutorProxy['Modelo']);
+console.log(tradutorProxy['Marca']);
 
