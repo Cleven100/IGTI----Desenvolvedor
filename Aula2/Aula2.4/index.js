5d964bebc42569471111eee4ce8321f30808b79b
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-03-29 11:16:12 -0300

    29/03

---
+++ Aula2/Aula2.4/index.js
@@ -0,0 +1,24 @@
+//PROXY COM REFLECT
+let tradutor = {
+    'Carro': 'Car',
+    'Ano'  : 'Year'
+};
+
+let handlerTradutor = {
+    get(target, property){
+        if(property in target){
+            return Reflect.get(target, property)
+        } else { 
+            return property;
+        }
+    },
+    set(target, property, value){
+      if(typeof value == 'string'){
+          target[property] = value;
+      } else {
+          return false;
+      }
+    }
+};
+
+tradutor = new Proxy(tradutor, handlerTradutor);
