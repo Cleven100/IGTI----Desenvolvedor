78066c66423172ba78ac55ef7b0ea15a5f74652a
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-03-29 14:55:56 -0300

    29/03

--- Aula3/Aula3.2/index.js
+++ Aula3/Aula3.2/index.js
@@ -9,8 +9,20 @@ Promise.all([
     
     new Promise(resolve => setTimeout(resolve, 1200, "p1")),
     new Promise(resolve => setTimeout(resolve, 1200, "p2")),
-    new Promise(resolve => setTimeout(resolve, 1200, "p3")),
+    new Promise(resolve => setTimeout(resolve, 1200, "p3"))
 
 ]).then(results => results.data[0].name)
 .then(name => console.info(name))
 .catch(error => console.error(`exceção lançada na: ${error}`))
+
+
+Promise.all([
+     
+      
+    new Promise(resolve => setTimeout(() => resolve([]), 1200)),
+    new Promise(resolve => setTimeout(() => resolve([10]), 1200)),
+    new Promise(resolve => setTimeout(() => resolve([3,4]), 1200))
+
+]).then(results => results.length)
+.then(size => console.info(size))
+.catch(erro => console.error(erro));
