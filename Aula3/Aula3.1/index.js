6a3a29fc5c1e57cf4c349f8d6d6e25d1f91af813
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-03-29 13:29:46 -0300

    29/03

--- Aula3/Aula3.1/index.js
+++ Aula3/Aula3.1/index.js
@@ -1,5 +1,5 @@
 const p1 = new Promise((resolve,reject) =>{
-    setTimeout(() => resolve('sucesso'), 2000)
+    setTimeout(() => resolve('sucesso P1'), 2000)
 })
 
 p1.then((res) => {console.log(res)}, (rej) => {});
@@ -7,5 +7,16 @@ p1.then((res) => {console.log(res)}, (rej) => {});
 
 
 new Promise((resolve,reject) =>{
-    setTimeout(() => resolve('sucesso'), 2000)
+    setTimeout(() => resolve('sucesso P1'), 2000)
 }).then((res) => {console.log(res)}, (resj) => {});
+
+
+
+const p2 = new Promise((resolve,reject) =>{
+    setTimeout(() => resolve('sucesso P2'), 2000)
+})
+
+p2.then((res) => {console.log(res)});
+p2.catch((rej) => {});
+
+p2.then((res) => {console.log(res)}).catch((rej) => {})
