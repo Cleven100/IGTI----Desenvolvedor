3fa609bfe68cf705ffc8406205f12d1e342f030a
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-04-05 14:22:38 -0300

    05/04

--- Aula5/Aula5.1/index.js
+++ Aula5/Aula5.1/index.js
@@ -26,3 +26,20 @@ console.log(_.random(10));
 
 console.log(_.random(5,1000))
 
+_.times(3, () => {
+    console.log("repetição");
+})
+
+
+_.delay(() => {console.log("Atrasada")}, 1500)
+
+
+let a = 1;
+let b = 'carro';
+let c = [1,2];
+let d = {name: 'koaok'};
+
+console.log(_.isNumber(a));
+console.log(_.isString(b));
+console.log(_.isArray(c));
+console.log(_.isObject(d));
