b5f47b5cb26de431270d350264a5097cebba1936
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-04-05 14:07:53 -0300

    05/04

--- Aula5/Aula5.1/index.js
+++ Aula5/Aula5.1/index.js
@@ -7,3 +7,7 @@ let cars = ['Palio', 'Uno', 'Gol', 'Saveiro', 'HB20', 'Creta'];
 
 console.log(_.first(cars));
 console.log(_.last(cars));
+
+console.log(_.nth(cars,2));
+
+console.log(_.nth(cars,-4))
