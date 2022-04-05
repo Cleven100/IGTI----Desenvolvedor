78a1c6e45a618178035d9aaf23153bac9dd64744
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-04-05 15:22:01 -0300

    05/04

--- Aula5/Aula5.2/index.js
+++ Aula5/Aula5.2/index.js
@@ -1,5 +1,5 @@
 import {format} from 'date-fns'
-
+import { es, pt } from 'date-fns/locale/index.js'
 
 let today = new Date();
 
@@ -8,3 +8,14 @@ console.log(today);
 
 let todayFormatted = format(today, 'dd.MM.yyyy');
 console.log(todayFormatted);
+
+let todayFormatted2 = format(today, 'PPPP');
+console.log(todayFormatted2);
+
+
+let todayFormatted3 = format(today, 'PPPP', {locale: es});
+console.log(todayFormatted3);
+
+let todayFormatted4 = format(today, 'PPPP', {locale: pt});
+console.log(todayFormatted4);
+
