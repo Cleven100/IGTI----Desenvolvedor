afd82fbf38e704fc34a4310e2921b89647c92a8f
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-04-28 12:54:08 -0300

    28/04

--- Node/Modulo1/LeituraTXT/index.js
+++ Node/Modulo1/LeituraTXT/index.js
@@ -1,11 +1,38 @@
 import fs from "fs";
 
-const teste = "Bla bla blu ble dlo"
+const teste = "15, 16, 17, 20"
+const novo = ", 25, 16, 18, 32"
 
 fs.writeFile("teste.txt", teste, erro => {
    if(erro){
        console.log(erro)
    } else {
-       console.log("arquivo com sucesso");
+       
+      fs.appendFile("teste.txt", novo, erro => {
+          if(erro)
+          {
+              console.log(erro)
+          } else {
+               fs.readFile("teste.txt", "utf-8", (erro, data) => {
+           if(erro){
+               console.log(erro)
+           } else {
+               const dados = data;
+               imprimir(dados);
+           }
+       })
+          }
+      })
+     
+
+      
    }
 })
+
+const imprimir = (dados) => {
+    let inteiros = JSON.parse("["+dados+"]");
+    let valorMaior = inteiros.filter((dado) => dado >= 18).sort();
+   console.log(valorMaior);
+}
+
+
