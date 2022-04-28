aa6679bf48cb4666d22d39445d5cd46f410d9e73
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-04-28 13:48:33 -0300

    28/04

--- Node/Modulo1/LeituraTXT/index.js
+++ Node/Modulo1/LeituraTXT/index.js
@@ -1,7 +1,45 @@
-import fs from "fs";
+ import {promises as fs} from "fs";
 
-const teste = "15, 16, 17, 20"
-const novo = ", 25, 16, 18, 32"
+teste();
+
+ async function teste() {
+    try {
+       await fs.writeFile("teste.txt", "14,20,30,50");
+          await     fs.appendFile("teste.txt", "\n13, 15, 45");
+            const data = await  fs.readFile("teste.txt", "utf-8")
+                    console.log(data);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
+
+
+ /*fs.writeFile("teste.txt", "14,20,30,50").then(() => {
+   fs.appendFile("teste.txt", "\n13, 15, 45").then(() =>{
+     fs.readFile("teste.txt", "utf-8").then(response =>{
+       console.log(response);
+     }).catch(error => {
+         console.log(error)
+     })
+   }).catch(error => {
+       console.log(error)
+   })
+ }).catch(error =>{
+     console.log(error);
+ })*/
+ 
+ 
+ 
+ 
+ 
+ 
+ /*import fs from "fs";
+
+const teste = " 15 16 17 20"
+const novo =  " 25 16 18 32"
 
 fs.writeFile("teste.txt", teste, erro => {
    if(erro){
@@ -30,9 +68,15 @@ fs.writeFile("teste.txt", teste, erro => {
 })
 
 const imprimir = (dados) => {
-    let inteiros = JSON.parse("["+dados+"]");
-    let valorMaior = inteiros.filter((dado) => dado >= 18).sort();
-   console.log(valorMaior);
+   
+
+    let novoValor = dados.replace("", ",");
+   // let inteiros = JSON.parse("["+dados+"]");
+    
+    //let valorMaior = inteiros.filter((dado) => dado >= 18).sort();
+   console.log(novoValor);
 }
+*/
+
 
 
