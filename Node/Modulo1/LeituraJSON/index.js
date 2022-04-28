6e370c0b68e06d0df570902e023e35f13ae1bcac
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-04-28 15:23:51 -0300

    28/04

--- Node/Modulo1/LeituraJSON/index.js
+++ Node/Modulo1/LeituraJSON/index.js
@@ -1,1 +1,20 @@
-console.log("Hello world")
+import {promises as fs} from "fs";
+
+
+writeReadJson();
+
+async function writeReadJson() {
+ try {
+    const arrayCarros = [ "Gol", "Palio", "Uno" ];
+    const obj = {
+            carros: arrayCarros
+        };
+     await fs.writeFile("teste.json",JSON.stringify(obj));
+     const data = JSON.parse(await (fs.readFile("teste.json")));
+     
+     data.carros.push("Sandeiro");
+     await fs.writeFile("teste.json", JSON.stringify(data));
+ } catch (error) {
+    console.log(error); 
+ }
+}
