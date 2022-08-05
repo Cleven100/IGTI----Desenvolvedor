3c01026dbf4206036de21b63c1cb33055b110109
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-08-05 18:31:45 -0300

    useState

--- devReact/Modulo - 1/react-hello/src/App.js
+++ devReact/Modulo - 1/react-hello/src/App.js
@@ -1,17 +1,28 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 
 export default function App() {
-  console.log('Teste no console do navegador');
+
+  const [name, setName] = useState('Cleven');
+
+  function handleNameChange(event) {
+    const newName = event.currentTarget.value;
+    setName(newName)
+  }
 
   return (
     <>
-     <Header size="large">Component Header - hello-react</Header> 
-     <Main>
-       <input className="border" type="text" />
-       <p>Meu nome é teste 52</p>
-     </Main>
-  
+      <Header size="large">Component Header - hello-react</Header>
+      <Main>
+        <div className="flex flex-col my-4">
+          <label className="text-sm mb-1" htmlFor="inputName">Digite o seu nome: </label>
+          <input autoFocus id="inputName" className="border p-1" type="text" value={name} onChange={handleNameChange}/>
+        </div>
+
+        <p>Meu nome é {name}</p>
+      </Main>
+
     </>
   )
 }
